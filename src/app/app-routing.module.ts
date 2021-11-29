4c5b398a6e68193ee6a94059174a613645d99c98
Andrii-25 <kotsupyr.a@gmail.com>
2021-11-29 22:33:06 +0200

    add team-form component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TeamsComponent } from './teams/teams.component';
 import { PlayersComponent } from './players/players.component';
+import { TeamFormComponent } from './team-form/team-form.component';
 
 const routes: Routes = [
   { path: 'teams', component: TeamsComponent },
   { path: 'players', component: PlayersComponent },
+  { path: 'add-team', component: TeamFormComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,13 @@ import { MatTableModule } from '@angular/material/table';
 import { TeamsComponent } from './teams/teams.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatButtonModule } from '@angular/material/button';
 
 import { HttpClientModule } from '@angular/common/http';
 import { PlayersComponent } from './players/players.component';
 import { RouterModule } from '@angular/router';
+import { TeamFormComponent } from './team-form/team-form.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -22,6 +24,7 @@ import { RouterModule } from '@angular/router';
     ToolbarComponent,
     TeamsComponent,
     PlayersComponent,
+    TeamFormComponent,
   ],
   imports: [
     BrowserModule,
@@ -34,6 +37,7 @@ import { RouterModule } from '@angular/router';
     MatPaginatorModule,
     MatButtonModule,
     RouterModule,
+    MatFormFieldModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
